edda987bb6a5984f47110741c61f1c174ff8634e
AnOddRadish <anoddradish@gmail.com>
2015-12-01 12:41:34 -0500

    First Challenge
    Complete first challenge

--- Challenge1.cpp
+++ Challenge1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+int main() {
+	for (int i = 1; i <= 100; i++){
+		if (i % 3 == 0){
+			std::cout << "Fire";
+		}
+		if (i % 7 == 0 && i % 3 == 0){
+			std::cout << " and ";
+		}
+		if (i % 7 == 0){
+			std::cout << "Ice";
+		}
+		if (!((i % 3 == 0) || (i % 7 == 0))){
+			std::cout << i;
+		}
+		std::cout << std::endl;
+	}
+}
